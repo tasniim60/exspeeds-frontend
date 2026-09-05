be8ad4d6ef6bc95711c2d714213ad532e6af646a
tasniim60 <tasniime60@gmail.com>
2026-09-06 00:07:10 +0300

    refactor: remove unused dummy components, organic-login route, and scratch scripts

--- scripts/check_keys.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-// Dynamically read en.ts and ar.ts or inspect their object keys
-const enContent = fs.readFileSync(path.join(__dirname, '../src/locales/en.ts'), 'utf8');
-const arContent = fs.readFileSync(path.join(__dirname, '../src/locales/ar.ts'), 'utf8');
-
-console.log("en.ts length:", enContent.length, "ar.ts length:", arContent.length);
